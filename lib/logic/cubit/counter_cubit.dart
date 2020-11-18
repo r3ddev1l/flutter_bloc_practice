1b92d8a9290f8c9ae36d8482a726056bfb567f83
srijan <srijan@mavorion.com>
2020-11-18 17:48:56 +0545

    bloc to bloc communication

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,15 +1,44 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc_concepts/constants/enum.dart';
+import 'package:flutter_bloc_concepts/logic/cubit/internet_cubit.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0));
+  final InternetCubit internetCubit;
+
+  StreamSubscription internetStreamSubscription;
+
+  CounterCubit({@required this.internetCubit})
+      : super(CounterState(counterValue: 0)) {
+    monitorInternetCubit();
+  }
+
+  StreamSubscription<InternetState> monitorInternetCubit() {
+    return internetStreamSubscription = internetCubit.listen((internetState) {
+      if (internetState is InternetConnected &&
+          internetState.connectionType == ConnectionType.Wifi) {
+        increment();
+      } else if (internetState is InternetConnected &&
+          internetState.connectionType == ConnectionType.Mobile) {
+        decrement();
+      }
+    });
+  }
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
+
+  @override
+  Future<void> close() {
+    internetStreamSubscription.cancel();
+    return super.close();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_concepts/logic/cubit/internet_cubit.dart';
 import 'package:flutter_bloc_concepts/presentation/router/app_router.dart';
 
 void main() {
   runApp(
-    MyApp(),
+    MyApp(
+      appRouter: AppRouter(),
+      connectivity: Connectivity(),
+    ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  final AppRouter _appRouter = AppRouter();
+  final Connectivity connectivity;
+  final AppRouter appRouter;
 
+  MyApp({@required this.appRouter, @required this.connectivity});
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<InternetCubit>(
+          create: (context) => InternetCubit(connectivity: connectivity),
+        ),
+        BlocProvider<CounterCubit>(
+          create: (context) =>
+              CounterCubit(internetCubit: context.bloc<InternetCubit>()),
+        ),
+      ],
       child: MaterialApp(
         title: 'Bloc Demo',
-        onGenerateRoute: _appRouter.onGenerateRoute,
+        onGenerateRoute: appRouter.onGenerateRoute,
       ),
     );
   }
